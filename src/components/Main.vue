ada526ac76d54e55e5acec93b4bc78e0fdc97ef9
bryannbarbosa <bryannbarbosa@gmail.com>
2018-03-06 21:53:50 -0300

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <Navbar v-for="(navbar, i) in navbars" :key="i" :color="navbar.color" :config="navbar.config" :items="navbar.items"></Navbar>
+    <div v-bind:class="{ 'container-fluid': fluid }">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+
+export default {
+  name: 'Main',
+  data () {
+    return {
+      fluid: true,
+      columns: [
+        {
+          type: 'sm',
+          size: 6
+        }
+      ],
+      navbars: [{
+        items: [{
+          name: 'Active',
+          href: '#',
+          active: true
+        }, {
+          name: 'Disabled',
+          href: '#'
+        }, {
+          name: 'About Us',
+          href: '#'
+        }],
+        color: 'blue',
+        config: {
+          title: 'Personalize o Menu',
+          content: '<button class="btn btn-primary">Adicionar Item</button>',
+          placement: 'bottom',
+          target: 'popoverButton-open'
+        }
+      }]
+    }
+  },
+  methods () {
+    return {
+    }
+  },
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav v-b-popover.hover.html="config" variant="success" class="navbar navbar-expand-sm bg-dark navbar-dark" v-bind:class="[color, textcolor]">
+    <ul class="navbar-nav">
+      <li v-for="(item, i) in items" :key="i" class="nav-item">
+        <a class="nav-link" v-bind:class="{ 'active': item.active }" v-bind:href="item.href">{{ item.name }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: ['items', 'color', 'textcolor', 'config'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.red {
+  background-color: red !important;
+}
+.green {
+  background-color: green !important;
+}
+
+.black {
+  background-color: black !important;
+}
+
+</style>
